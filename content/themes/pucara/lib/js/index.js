83589530df811d011c97aa90755cb4366227abb6
Miguel Alejandro <mvinan@icloud.com>
2015-08-22 03:28:08 -0500

    WIP contact

--- content/themes/pucara/lib/js/index.js
+++ content/themes/pucara/lib/js/index.js
@@ -4,4 +4,14 @@
 
   $('.post:nth-child(1n+2)').wrapAll("<div class='others col-md-6 col-sm-12'></div>");
   $('.others .post').addClass('col-md-6 col-xs-12')
+
+  $(window).load(function(){
+    setTimeout(function(){
+      $('.container-logo').addClass('fadeIn')
+    }, 500)
+    setTimeout(function(){
+      $('.blog-description').addClass('fadeIn')
+      $('.btn-about').addClass('fadeIn')
+    }, 900)
+  });
 })();
