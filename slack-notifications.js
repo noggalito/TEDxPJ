f72a677ae98e2a647805bbed5d24c23ed066bac7
Miguel Alejandro <mvinan@icloud.com>
2015-09-10 12:18:29 -0500

    syntax fixed

--- slack-notifications.js
+++ slack-notifications.js
@@ -1,23 +1,24 @@
 'use strict'
-let Slack   = require('slack-node'),
+const Slack   = require('slack-node'),
     fs      = require('fs'),
     path    = require('path');
 
-let webhookUri       = process.env.SLACK_HOOK_URL,
+var webhookUri       = process.env.SLACK_HOOK_URL,
     projectUri       = process.env.SLACK_PROJECT_URL,
-    slackChannel     = process.env.SLACK_CHANNEL || "",
+    slackChannel     = process.env.SLACK_CHANNEL || "#general",
     projectName      = process.env.SLACK_PROJECT_NAME || "name project",
     iconNotify       = process.env.SLACK_PROJECT_ICON || ":squirrel:",
     slackEnvironment = process.env.SLACK_ENVIRONMENT || "production",
     userDeploy       = process.env.SLACK_USER_DEPLOY || "user",
     botName          = process.env.SLACK_BOT_NAME || "Squirrel";
 
-let filePath = path.join(__dirname, '../.deploys');
-let deploysRead = () => {
+var filePath = path.join(__dirname, '../.deploys');
+
+function deploysRead () {
   return fs.readFileSync(filePath, {encoding: 'utf-8'});
 }
 
-let lastVersionDeployed = () => {
+function lastVersionDeployed() {
   let deploysVersions, lastElement, lastVersion;
   deploysVersions = deploysRead().split('\n')
   lastElement = deploysVersions.length - 2
@@ -25,13 +26,13 @@ let lastVersionDeployed = () => {
   return lastVersion
 }
 
-let slack = new Slack();
+var slack = new Slack();
 slack.setWebhook(webhookUri);
 
 slack.webhook({
   channel   : slackChannel,
   username  : botName,
-  text: `<mailto:${userDeploy}|${userDeploy}> deployed version ${lastVersionDeployed()} application <${projectUri}|${projectName}> to ${slackEnvironment} environment`,
+  text: `<mailto:${userDeploy}|${userDeploy}> deployed version <http://github.com/noggalito/tedxpj/commits/|${lastVersionDeployed()}> application <${projectUri}|${projectName}> to ${slackEnvironment} environment`,
   icon_emoji: iconNotify
 },
   (err, response) => {
