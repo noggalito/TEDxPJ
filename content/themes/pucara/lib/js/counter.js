3820a99970e6f205a8aabd9dff05734ae9a34123
Mario Andrés Correa <a@macool.me>
2016-01-08 00:22:10 -0500

    update event date
    http://nodriza.shiriculapo.com/issues/41

--- content/themes/pucara/lib/js/counter.js
+++ content/themes/pucara/lib/js/counter.js
@@ -1,32 +1,35 @@
 (function(){
-	"use strict"
+	"use strict";
 
-	var end = new Date("11/06/2015")
-	var second = 1000
-	var minute = second * 60
-	var hour = minute * 60
-	var day = hour * 24
-	var timer
+	var selector = ".counter";
+
+	var end = new Date("05/27/2016");
+	var second = 1000;
+	var minute = second * 60;
+	var hour = minute * 60;
+	var day = hour * 24;
+	var timer;
 
 	function showRemaining() {
-		var now = new Date()
-		var distance = end - now
+		var now = new Date();
+		var distance = end - now;
 
 		if (distance < 0) {
-			clearInterval(timer)
-			$(".ready").html("TEDx ParqueJipiro es AHORA!!")
+			clearInterval(timer);
+			$(".ready").html("TEDx ParqueJipiro es AHORA!!");
 
-			return
+			return;
 		}
 
-		var days = Math.floor(distance / day)
-		var hours = Math.floor( (distance  % day) / hour )
-		var minutes = Math.floor((distance % hour ) / minute)
-		var seconds = Math.floor( (distance % minute) / second )
+		var days = Math.floor(distance / day);
+		var hours = Math.floor( (distance  % day) / hour );
+		var minutes = Math.floor((distance % hour ) / minute);
+		var seconds = Math.floor( (distance % minute) / second );
 
-		$(".counter").html("<div class='days'> <span>" + days + "</span><span>dias</span></div>" + "<div class='hours'><span>" + hours + " </span><span>horas</span> </div>" + "<div class='minutes'><span>" + minutes + " </span><span>minutos</span> </div>" + "<div class='seconds'><span>" + seconds + " </span><span>segundos</span> </div>")
+		$(selector).html("<div class='days'> <span>" + days + "</span><span>días</span></div>" + "<div class='hours'><span>" + hours + " </span><span>horas</span> </div>" + "<div class='minutes'><span>" + minutes + " </span><span>minutos</span> </div>" + "<div class='seconds'><span>" + seconds + " </span><span>segundos</span> </div>");
 	}
 
-	timer = setInterval(showRemaining, 1000)
-
+	if ($(selector).length > 0) {
+		timer = setInterval(showRemaining, 1000);
+	}
 })();
